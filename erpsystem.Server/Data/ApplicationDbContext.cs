39e4a041f3b191e3c366876d3959cd2b66c2345c
patrykplonka <123236364+patrykplonka@users.noreply.github.com>
2025-04-01 11:14:20 +0200

    Update in ApplicationDbContext.

--- erpsystem.Server/Data/ApplicationDbContext.cs
+++ erpsystem.Server/Data/ApplicationDbContext.cs
@@ -1,8 +1,39 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using erpsystem.Models;
+using erpsystem.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+namespace ERPSystem.API.Data
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<ApplicationUser> Users { get; set; } // Zmieniona nazwa tabeli
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Dodanie ról do bazy
+            builder.Entity<Role>().HasData(
+                new Role { RoleId = 1, Name = "Admin" },
+                new Role { RoleId = 2, Name = "User" }
+            );
+
+            // Konfiguracja unikalnego emaila
+            builder.Entity<ApplicationUser>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+
+            // Powiązanie użytkowników z rolami
+            builder.Entity<ApplicationUser>()
+                .HasOne(u => u.Role)
+                .WithMany()
+                .HasForeignKey(u => u.RoleId);
+        }
+    }
 }
