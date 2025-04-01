6fdf863bb5d18db77d84417033aa791da0882a9f
patrykplonka <123236364+patrykplonka@users.noreply.github.com>
2025-04-01 11:05:40 +0200

    Added Entity Framework Core configuration in Program.cs

--- erpsystem.Server/Program.cs
+++ erpsystem.Server/Program.cs
@@ -1,6 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
