a128c5cab7877e2265a030218fa23768020b6a02
patrykplonka <123236364+patrykplonka@users.noreply.github.com>
2025-04-01 11:13:35 +0200

    Added User and Role models.

---
+++ erpsystem.Server/Models/ApplicationUser.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
+
+namespace erpsystem.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public override string Email { get; set; } 
+
+        [Required]
+        public string PasswordHash { get; set; } 
+
+        [ForeignKey("Role")]
+        public int RoleId { get; set; }
+        public virtual Role Role { get; set; } 
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public UserStatus Status { get; set; } = UserStatus.Active;
+    }
+
+    public enum UserStatus
+    {
+        Active,
+        Blocked,
+        Inactive
+    }
+}
---
+++ erpsystem.Server/Models/Role.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace erpsystem.Models
+{
+    public class Role
+    {
+        [Key]
+        public int RoleId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+    }
+}
