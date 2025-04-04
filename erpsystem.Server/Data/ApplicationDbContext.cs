fd551171338cc60e2affa1306506ba5a2d288ba4
patrykplonka <123236364+patrykplonka@users.noreply.github.com>
2025-04-04 08:15:27 +0200

    Update ApllicationDbContext

--- erpsystem.Server/Data/ApplicationDbContext.cs
+++ erpsystem.Server/Data/ApplicationDbContext.cs
@@ -6,9 +6,10 @@ namespace erpsystem.Server.Data
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
-
+        public DbSet<WarehouseItem> WarehouseItems { get; set; }
     }
 }
