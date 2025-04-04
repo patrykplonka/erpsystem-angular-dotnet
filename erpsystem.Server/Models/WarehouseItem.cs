210d8290453797f8f632c824b7aee99ee796cda6
patrykplonka <123236364+patrykplonka@users.noreply.github.com>
2025-04-04 08:05:47 +0200

    Added WarehouseItem model.

---
+++ erpsystem.Server/Models/WarehouseItem.cs
@@ -0,0 +1,14 @@
+﻿namespace erpsystem.Server.Models
+{
+    public class WarehouseItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+        public string CreatedBy { get; set; }
+    }
+}
