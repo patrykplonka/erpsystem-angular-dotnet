aaeb6e7f1999a95689f3c7ce63baf29b3213b071
patrykplonka <123236364+patrykplonka@users.noreply.github.com>
2025-04-11 09:38:48 +0200

    Added DTO's models.

---
+++ erpsystem.Server/Models/DTOs/UpdateContractorDto.cs
@@ -0,0 +1,14 @@
+﻿namespace erpsystem.Server.Models.DTOs
+{
+    public class UpdateContractorDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Type { get; set; } = "Supplier";
+        public string Email { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string TaxId { get; set; } = string.Empty;
+        public bool IsDeleted { get; set; } = false;
+    }
+}
