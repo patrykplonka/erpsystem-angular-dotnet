de3b6e411e224aad1fa4de4a394f391d7725d0c1
patrykplonka <123236364+patrykplonka@users.noreply.github.com>
2025-04-22 08:00:32 +0200

    Added Purchase and Supplier model.

---
+++ erpsystem.Server/Models/Purchase.cs
@@ -0,0 +1,17 @@
+﻿namespace erpsystem.Server.Models
+{
+    public class Purchase
+    {
+        public int Id { get; set; }
+        public string PurchaseNumber { get; set; }
+        public int SupplierId { get; set; }
+        public Supplier Supplier { get; set; }
+        public string OrderDate { get; set; }
+        public decimal TotalAmount { get; set; }
+        public string Status { get; set; }
+        public string CreatedBy { get; set; }
+        public string CreatedDate { get; set; }
+        public bool IsDeleted { get; set; }
+        public List<PurchaseItem> PurchaseItems { get; set; }
+    }
+}
