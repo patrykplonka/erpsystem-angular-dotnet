47e247ee69bb5512e1880ecba6b42254c5a627eb
patrykplonka <123236364+patrykplonka@users.noreply.github.com>
2025-05-04 17:50:09 +0200

    Added LocationDto.

---
+++ erpsystem.Server/Models/DTOs/LocationDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace erpsystem.Server.Models.DTOs
+{
+    public class LocationDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nazwa jest wymagana")]
+        [StringLength(50, ErrorMessage = "Nazwa nie może przekraczać 50 znaków")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Typ jest wymagany")]
+        [StringLength(20, ErrorMessage = "Typ nie może przekraczać 20 znaków")]
+        public string Type { get; set; } = string.Empty; 
+    }
+}
