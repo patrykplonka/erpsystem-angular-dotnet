9002c5a93a916205271f9a50ced754fdefb37f4d
patrykplonka <123236364+patrykplonka@users.noreply.github.com>
2025-05-04 17:58:10 +0200

    Updated ApplicationDbContext.

--- erpsystem.Server/Data/ApplicationDbContext.cs
+++ erpsystem.Server/Data/ApplicationDbContext.cs
@@ -19,8 +19,19 @@ namespace erpsystem.Server.Data
         public DbSet<OrderHistory> OrderHistory { get; set; }
         public DbSet<OperationLog> OperationLogs { get; set; }
         public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<Location> Locations { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
 
-
+            modelBuilder.Entity<Location>().HasData(
+                new Location { Id = 1, Name = "Regał A1", Type = "Magazyn Główny" },
+                new Location { Id = 2, Name = "Regał B2", Type = "Magazyn Główny" },
+                new Location { Id = 3, Name = "Regał C3", Type = "Magazyn Główny" },
+                new Location { Id = 4, Name = "Regał D4", Type = "Magazyn Główny" },
+                new Location { Id = 5, Name = "Regał E5", Type = "Magazyn Główny" }
+            );
+        }
     }
 }
